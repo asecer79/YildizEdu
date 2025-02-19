390e967be3f0b929d96e3b8497705e6d5f9f1fb9
asecer <aydinsecer@gmail.com>
2025-02-19 10:53:42 +0300

    Add project files.

---
+++ Week1/Controllers/StudentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Week1.Models;
+
+namespace Week1.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            Student s1 = new Student()
+            {
+                Id = 1,
+                Name = "Ahmet Karaca",
+                Description = "Math Eng. Std.",
+                StudentId = "13546516516546"
+
+            };
+
+            return View(s1);
+        }
+    }
+}
