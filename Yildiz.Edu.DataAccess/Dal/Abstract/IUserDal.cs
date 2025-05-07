98f5f8bdc1915fa37c568ef16e64df75078ee2b8
asecer <aydinsecer@gmail.com>
2025-05-07 10:53:09 +0300

    u

--- Yildiz.Edu.DataAccess/Dal/Abstract/IUserDal.cs
+++ Yildiz.Edu.DataAccess/Dal/Abstract/IUserDal.cs
@@ -5,7 +5,6 @@ namespace Yildiz.Edu.DataAccess.Dal.Abstract
 {
     public interface  IUserDal: IEntityRepository<User>
     {
-       
         bool CheckUserToLogin(string email, string password);
 
         List<OperationClaim> GetUserOperationClaims(int userId);
